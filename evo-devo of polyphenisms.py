ed9834380de4d881cef73324af8d3dd7b2892c46
pengzug <pengzug@gmx.de>
2018-10-17 18:35:07 +0200

    Population: alpha, hill, phenotype

--- evo-devo of polyphenisms.py
+++ evo-devo of polyphenisms.py
@@ -26,67 +26,75 @@ A = 1       # scaling constant for deterministic factors
 B = 1 - A   # scaling constant for stochastic factors
 L = 1       # number of time steps per generation (= life span)
 R = 25      # relative timescale of environmental variation (no. of gen.s per environmental cycle)
-P = 0.2     # environmental predictability
-#alpha = 0  # strength of the feedback
-#hill = 1   # Hill coefficient
+P = 1     # environmental predictability
 
 def population(n, t):
     """creates a population of n individuals at generation t,
        where each individual receives an environmental cue C 
        which is correlated with environmental condition E"""
     
-    # E: environmental condition
+    # E: environmental condition (e.g. temperature)
     eps = np.random.uniform(0, 1)   # stochastic error term
     E = A * (np.sin(2 * np.pi * t / (L * R)) + 1) / 2 + B * eps
     print("Env value E: " + str(E))
     
-    # C: environmental cue
+    # C: environmental cue (e.g. photoperiod)
     mu = 0.5 * (1 - P) + P * E
     sigma = 0.5 * (1 - P) / 3
     C = np.random.normal(mu, sigma)
     print("Env cue C:   " + str(C))
     
     # pop: population
-    pop = np.zeros(n, dtype=[('alpha','<f8'), ('hill','<f8'), ('phenotype','<f8'), ('fitness','<f8')])
-    pop['hill'] = 1
-    #pop['alpha'] = 1
+    # alpha: strength of the feedback (in the ODE); initial value: 0
+    # hill: Hill coefficient (in the ODE); initial value: 1
+    pop = np.zeros(n, dtype=[('alpha','<f8'), ('hill','<f8'), ('phenotype','<f8')])
+    pop['alpha'] = np.random.rand(n) * 2        # 1.4
+    print("alpha: ", pop['alpha'])
+    pop['hill']  = np.random.randint(1, 11, n)  # 8
+    print("hill: ", pop['hill'])
+    def phenotype(n):
+        """creates the phenotype"""
+        def phenotype_ode(t_ode, x):
+            """defines the ode(s) for the phenotype"""
+            dxdt = C - x + pop['alpha'] * (x ** pop['hill'] / (1 + x ** pop['hill']))
+            return dxdt
+        t_end = 1e09
+        sol_lower = solve_ivp(phenotype_ode, [0, t_end], np.zeros(n), method='BDF')
+        sol_upper = solve_ivp(phenotype_ode, [0, t_end], np.ones(n) * 10, method='BDF')
+        lower_steady_states = sol_lower.y[:, -1]
+        upper_steady_states = sol_upper.y[:, -1]
+        print(lower_steady_states)
+        print(upper_steady_states)
+        ### WARNING: ALWAYS CHOOSING UPPER STEADY STATE ###
+        return upper_steady_states
+    pop['phenotype'] = phenotype(n)
+    return pop
     
-    def phenotype_ode(ta, x):
-        """defines the differential equation(s)"""
-        dxdt = C - x + pop['alpha'] * (x ** pop['hill'] / (1 + x ** pop['hill']))
-        return dxdt
-    t_end = 1e06
-    odin(nu) = solve_ivp(phenotype_ode, [0, t_end], [0, 1], method='BDF')
     
     """
-    print(odin.y[0])
-    print(odin.y[1])
-    last_diff_lower = abs(odin.y[0][-1] - odin.y[0][-2])
-    last_diff_upper = abs(odin.y[1][-1] - odin.y[1][-2])
+    print(sol.y[0])
+    print(sol.y[1])
+    last_diff_lower = abs(sol.y[0][-1] - sol.y[0][-2])
+    last_diff_upper = abs(sol.y[1][-1] - sol.y[1][-2])
     #print("last_diff_lower: " + str(last_diff_lower))
     #print("last_diff_upper: " + str(last_diff_upper))
     if (last_diff_lower < 1e-7) != 1:
         print("Could not find the lower equilibrium! Probably t_end too low...")
     if (last_diff_upper < 1e-7) != 1:
         print("Could not find the upper equilibrium! Probably t_end too low...")
-    steady_state_lower = odin.y[0][-1]
-    steady_state_upper = odin.y[1][-1]
-    print("lower equi:  " + str(steady_state_lower))
-    print("upper equi:  " + str(steady_state_upper))
-    diff_upper_lower = abs(steady_state_lower - steady_state_upper)
+    lower_equilibrium = sol.y[0][-1]
+    upper_equilibrium = sol.y[1][-1]
+    print("lower equi:  " + str(lower_equilibrium))
+    print("upper equi:  " + str(upper_equilibrium))
+    diff_upper_lower = abs(lower_equilibrium - upper_equilibrium)
     if (diff_upper_lower < 1e-05) != 1:
         print("steady states do not match!")
-    pop['phenotype'] = steady_state_lower
-    
-    #def fitness_function():
-    
-    return pop['phenotype']
+    pop['phenotype'] = lower_equilibrium
     """
 
-#print(population(1, 24))
+n = 4
+t = 2
 
-t = 4
-#for t in range(3):
-print("Generation: " + str(t))
-print("Phenotype:  " + str(population(1, t)))
-print()
+print("Population size: ", n)
+print("Generation: ", t)
+print("Population:  ", population(4, t))
--- testground.py
+++ testground.py
@@ -7,18 +7,36 @@ Created on Wed Sep 19 14:29:33 2018
 """
 
 import numpy as np
-#from scipy.integrate import solve_ivp
+from scipy.integrate import solve_ivp
 
 def create_population(n):
-    pop = np.zeros(n, dtype=[('alpha','<f8'),('hill','<f8'),('phenotype','<f8')])
-    pop['alpha'] = np.random.randn(n)
-    pop['hill'] = 7
-    pop['phenotype'] = 2 * pop['alpha'] + pop['hill']
-    #for f in pop.dtype.fields:
-    #    pop[f] = np.random.randn(n)#*5
+    pop = np.zeros(n, dtype=[('alpha','<f8'),('beta','<f8'),('phenotype','<f8')])
+    pop['alpha'] = np.random.rand(n) * 2
+    print("alpha: ", pop['alpha'])
+    pop['beta'] = np.random.randint(1, 11, n)
+    print("beta: ", pop['beta'])
+    def phenotype(n):
+        #phenotype_simple = 2 * pop['alpha'] + pop['beta']
+        #print("alpha length: " + str(len(pop['alpha'])))
+        #print("beta length :" + str(len(pop['beta'])))
+        #return phenotype_simple
+        def phenotype_ode(t_ode, y):
+            dydt = 0.5 - y + pop['alpha'] * (y ** pop['beta'] / (1 + y ** pop['beta']))
+            #print(len(y))
+            #print("alpha length: " + str(len(pop['alpha'])))
+            #print("beta length :" + str(len(pop['beta'])))
+            return dydt
+        t_end = 1e09
+        sol_lower = solve_ivp(phenotype_ode, [0, t_end], np.zeros(n), method='BDF')
+        sol_upper = solve_ivp(phenotype_ode, [0, t_end], np.ones(n) * 10, method='BDF')
+        lower_steady_states = sol_lower.y[:, -1]
+        upper_steady_states = sol_upper.y[:, -1] # last entry is assumed to be the steady state
+        print("lower st st: ", lower_steady_states)
+        print("upper st st: ", upper_steady_states)
+        ### WARNING: ALWAYS CHOOSING UPPER STEADY STATE ###
+        return upper_steady_states 
+    pop['phenotype'] = phenotype(n)
     return pop
 
-pop = create_population(10)
-print(pop)
-odie = pop['phenotype']
-print(odie)
+popul = create_population(4)
+print(popul)
